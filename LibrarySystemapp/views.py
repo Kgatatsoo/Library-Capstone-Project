ab5a65a484c29d801e3d6820c4f096cfc91da8b3
kgatatsoo <vibrationbanda@gmail.com>
2026-01-03 02:47:25 +0200

    Created Superuser

--- LibrarySystemapp/views.py
+++ LibrarySystemapp/views.py
@@ -1,10 +1,21 @@
 
 from django.shortcuts import render
+from .models import Book
 
 def helloView(request):
     return render(request, "viewbook.html") 
 
 
 def addBookView(request):
-    return render(request, "addbook.html") 
+    if request.method=="POST":
+        t=request.POST["tittle"]
+        p=request.POST["price"]
+
+        book=Book()
+        book.title=t
+        book.price=p
+        book.save()
+        return HttpResponse('/add-book')
+
+    return render(request, "addbook.html",) 
 
